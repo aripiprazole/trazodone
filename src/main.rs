425e7cc20bb7a994d8a7fadd38a1fc173c429b32
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-04-02 17:44:35 -0300

    feat: add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::fs::File;
+
 fn main() {
-    println!("Hello, world!");
+    let example = std::fs::read_to_string("example.hvm").unwrap();
+    let file = hvm::language::syntax::read_file(&example).unwrap();
+    let rb = hvm::language::rulebook::gen_rulebook(&file);
+
+    println!("Hello, world! {rb:?}");
+}
+
+pub struct Codegen {
+
 }
