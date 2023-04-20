9926b8fe57c42b27e1b555f75e36b0c6e65d638b
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2023-04-20 16:14:49 -0300

    refactor: break in functions

--- src/codegen/apply/term.rs
+++ src/codegen/apply/term.rs
@@ -10,117 +10,14 @@ impl Codegen {
         match term {
             U60(u60) => Term::create_u60(u60),
             F60(f60) => Term::create_f60(f60),
-            Let(syntax::Let {
-                box value,
-                box body,
-                ..
-            }) => {
-                let value = self.build_term(value);
-                self.variables.push(value);
-                let body = self.build_term(body);
-                self.variables.pop();
-
-                body
+            Let(let_expr) => self.build_let(*let_expr.value, *let_expr.body),
+            Atom(atom_expr) => {
+                self.build_atom(atom_expr.name, atom_expr.index, atom_expr.field_index)
             }
-            App(syntax::App {
-                is_function: true,
-                arguments,
-                global_name: Some(global_name),
-                ..
-            }) => {
-                let name = self.fresh_name("call");
-                let value = self.alloc(arguments.len() as u64);
-                self.instructions.push(Instruction::binding(&name, value));
-
-                let compiled_global_name = build_name(&global_name);
-                let arguments = arguments
-                    .into_iter()
-                    .map(|argument| self.build_term(argument))
-                    .collect::<Vec<_>>();
-
-                for (index, argument) in arguments.iter().enumerate() {
-                    self.instructions.push(Instruction::link(
-                        Position::new(&name, index as u64),
-                        argument.clone(),
-                    ))
-                }
-
-                let index = self
-                    .global
-                    .constructors
-                    .get(&compiled_global_name)
-                    .unwrap_or_else(|| panic!("no function for {}", compiled_global_name));
-
-                Term::create_function(
-                    FunctionId::new(&compiled_global_name, *index),
-                    Position::initial(&name),
-                )
-            }
-            App(syntax::App {
-                is_function: false,
-                arguments,
-                global_name: Some(global_name),
-                ..
-            }) => {
-                let name = self.fresh_name("constructor");
-
-                let compiled_global_name = build_name(&global_name);
-                let arguments = arguments
-                    .into_iter()
-                    .map(|argument| self.build_term(argument))
-                    .collect::<Vec<_>>();
-
-                let value = self.alloc(arguments.len() as u64);
-
-                self.instructions.push(Instruction::binding(&name, value));
-
-                for (index, argument) in arguments.iter().enumerate() {
-                    self.instructions.push(Instruction::link(
-                        Position::new(&name, index as u64),
-                        argument.clone(),
-                    ))
-                }
-
-                let index = self
-                    .global
-                    .constructors
-                    .get(&compiled_global_name)
-                    .unwrap_or_else(|| panic!("no constructor for {}", compiled_global_name));
-
-                Term::create_constructor(
-                    FunctionId::new(&compiled_global_name, *index),
-                    Position::initial(&name),
-                )
-            }
-            App(syntax::App {
-                box callee,
-                arguments,
-                ..
-            }) => {
-                let name = self.fresh_name("app");
-                let callee = self.build_term(callee);
-                let argument = self.build_term(arguments.first().unwrap().clone());
-
-                let done = self.alloc(2);
-                self.instructions.push(Instruction::binding(&name, done));
-                self.instructions
-                    .push(Instruction::link(Position::initial(&name), callee));
-                self.instructions
-                    .push(Instruction::link(Position::new(&name, 1), argument));
-
-                Term::create_app(Position::initial(&name))
-            }
-            Atom(syntax::Atom {
-                name,
-                index,
-                field_index,
-            }) => match self.variables.get(index as usize) {
-                Some(value) => value.clone(),
-                None => Term::NotFound(syntax::Atom {
-                    name,
-                    index,
-                    field_index,
-                }),
+            App(app) => match app.global_name {
+                Some(global_name) if app.is_function => self.build_call(app.arguments, global_name),
+                Some(global_name) => self.build_constructor(app.arguments, global_name),
+                None => self.build_app(*app.callee, app.arguments),
             },
             Duplicate(_) => todo!(),
             Lam(_) => todo!(),
@@ -128,5 +25,102 @@ impl Codegen {
             Binary(_) => todo!(),
             Ref(_) => todo!(),
         }
+    }
+
+    fn build_atom(&mut self, name: String, index: u64, field_index: Option<u64>) -> Term {
+        match self.variables.get(index as usize) {
+            Some(value) => value.clone(),
+            None => Term::NotFound(syntax::Atom {
+                name,
+                index,
+                field_index,
+            }),
+        }
+    }
+
+    fn build_let(&mut self, value: syntax::Term, body: syntax::Term) -> Term {
+        let value = self.build_term(value);
+        self.variables.push(value);
+        let body = self.build_term(body);
+        self.variables.pop();
+
+        body
+    }
+
+    fn build_app(&mut self, callee: syntax::Term, arguments: Vec<syntax::Term>) -> Term {
+        let name = self.fresh_name("app");
+        let callee = self.build_term(callee);
+        let argument = self.build_term(arguments.first().unwrap().clone());
+
+        let done = self.alloc(2);
+        self.instructions.push(Instruction::binding(&name, done));
+        self.instructions
+            .push(Instruction::link(Position::initial(&name), callee));
+        self.instructions
+            .push(Instruction::link(Position::new(&name, 1), argument));
+
+        Term::create_app(Position::initial(&name))
+    }
+
+    fn build_constructor(&mut self, arguments: Vec<syntax::Term>, global_name: String) -> Term {
+        let name = self.fresh_name("constructor");
+
+        let compiled_global_name = build_name(&global_name);
+        let arguments = arguments
+            .into_iter()
+            .map(|argument| self.build_term(argument))
+            .collect::<Vec<_>>();
+
+        let value = self.alloc(arguments.len() as u64);
+
+        self.instructions.push(Instruction::binding(&name, value));
+
+        for (index, argument) in arguments.iter().enumerate() {
+            self.instructions.push(Instruction::link(
+                Position::new(&name, index as u64),
+                argument.clone(),
+            ))
+        }
+
+        let index = self
+            .global
+            .constructors
+            .get(&compiled_global_name)
+            .unwrap_or_else(|| panic!("no constructor for {}", compiled_global_name));
+
+        Term::create_constructor(
+            FunctionId::new(&compiled_global_name, *index),
+            Position::initial(&name),
+        )
+    }
+
+    fn build_call(&mut self, arguments: Vec<syntax::Term>, global_name: String) -> Term {
+        let name = self.fresh_name("call");
+        let value = self.alloc(arguments.len() as u64);
+        self.instructions.push(Instruction::binding(&name, value));
+
+        let compiled_global_name = build_name(&global_name);
+        let arguments = arguments
+            .into_iter()
+            .map(|argument| self.build_term(argument))
+            .collect::<Vec<_>>();
+
+        for (index, argument) in arguments.iter().enumerate() {
+            self.instructions.push(Instruction::link(
+                Position::new(&name, index as u64),
+                argument.clone(),
+            ))
+        }
+
+        let index = self
+            .global
+            .constructors
+            .get(&compiled_global_name)
+            .unwrap_or_else(|| panic!("no function for {}", compiled_global_name));
+
+        Term::create_function(
+            FunctionId::new(&compiled_global_name, *index),
+            Position::initial(&name),
+        )
     }
 }
